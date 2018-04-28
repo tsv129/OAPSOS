8cccfe0da906f287f71e38e2e74b8a8ca4e98ea5
tsv129 <32687515+tsv129@users.noreply.github.com>
2018-04-29 00:55:58 +0300

    Lab 2_3

---
+++ OAPSOS_lab/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace OAPSOS_lab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeight = 35;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.Location = new System.Drawing.Point(186, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 5;
+            this.dataGridView1.RowTemplate.Height = 35;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(402, 176);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Посчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(168, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(36, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(36, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 90);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(36, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 116);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(36, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 142);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(36, 20);
+            this.textBox5.TabIndex = 7;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(12, 168);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(36, 20);
+            this.textBox6.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 202);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OAPSOS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ OAPSOS_lab/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OAPSOS_lab
+{
+    public partial class Form1 : Form
+    {
+        int X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, X11, X12, X13, X14, bn, sn;
+        string bigN, smlN, tmpN;
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int c=0; c < 4; c++)
+                for (int r=0; r < 5; r++)
+                {
+                    dataGridView1[c, r].Value = String.Empty;
+                }
+
+            X1 = dateTimePicker1.Value.Day;
+            X2 = (X1 % 10);
+            X1 = (X1 / 10);
+            X3 = dateTimePicker1.Value.Month;
+            X4 = (X3 % 10);
+            X3 = (X3 / 10);
+            X5 = dateTimePicker1.Value.Year;
+            X6 = (X5 % 1000) / 100;
+            X7 = (X5 % 100) / 10;
+            X8 = (X5 % 10);
+            X5 = (X5 / 1000);
+            X9 = X1 + X2 + X3 + X4 + X5 + X6 + X7 + X8;
+            X10 = (X9 % 10) + (X9 / 10);
+            X11 = X9 - (X1 * 2);
+            X12 = (X11 % 10) + (X11 / 10);
+            X13 = X9 + X11;
+            X14 = X10 + X12;
+
+            textBox1.Text = X9.ToString();
+            textBox2.Text = X10.ToString();
+            textBox3.Text = X11.ToString();
+            textBox4.Text = X12.ToString();
+            textBox5.Text = X13.ToString();
+            textBox6.Text = X14.ToString();
+
+            bigN = dateTimePicker1.Value.ToShortDateString() + textBox1.Text +
+                textBox2.Text + textBox3.Text + textBox4.Text;
+            smlN = textBox5.Text + textBox6.Text;
+
+            Calc(0);
+            dataGridView1[3, 0].Value = bn.ToString() + '(' + sn.ToString() + ')';
+
+            for (int i = 0; i < 3; i++)
+            {
+                bn = 0; sn = 0;
+                for (int j = 1; j < 4; j++)
+                    Calc(i * 3 + j);
+                if (bn > 12)
+                    bn = (bn % 10) + (bn / 10);
+                if (sn > 12)
+                    sn = (sn % 10) + (sn / 10);
+                dataGridView1[3, i + 1].Value = bn.ToString() + '(' + sn.ToString() + ')';
+            }
+
+            for (int i = 10; i < 13; i++)
+            {
+                bn = 0; sn = 0;
+                if ((i == X13) || (i == X14))
+                {
+                    switch (i)
+                    {
+                        case 10:
+                            dataGridView1[0, 4].Value = "(10)";
+                            break;
+                        case 11:
+                            dataGridView1[1, 4].Value = "(11)";
+                            break;
+                        case 12:
+                            dataGridView1[2, 4].Value = "(12)";
+                            break;
+                    }
+                    dataGridView1[3, 4].Value = '(' + i.ToString() + ')';
+                }
+            }
+        }
+
+        private void Calc(int num)
+        {
+            char c = char.Parse(num.ToString());
+            int i = 0, j = 0, k = 0;
+            for (i = 0; i < bigN.Length; i++)
+                if (bigN[i].Equals(c))
+                    j++;
+            bn = bn + (j * num);
+
+            for (i = 0; i < smlN.Length; i++)
+                if (smlN[i].Equals(c))
+                    k++;
+            sn = sn + (k * num);
+
+            for (i = 0; i < j; i++)
+                tmpN = tmpN + c;
+
+            if (k != 0)
+            {
+                tmpN = tmpN + '(';
+                for (i = 0; i < k; i++)
+                    tmpN = tmpN + c;
+                tmpN = tmpN + ')';
+            }
+
+            switch (num)
+            {
+                case 0:
+                    dataGridView1[1, 0].Value = tmpN;
+                    break;
+                case 1:
+                    dataGridView1[0, 1].Value = tmpN;
+                    break;
+                case 2:
+                    dataGridView1[1, 1].Value = tmpN;
+                    break;
+                case 3:
+                    dataGridView1[2, 1].Value = tmpN;
+                    break;
+                case 4:
+                    dataGridView1[0, 2].Value = tmpN;
+                    break;
+                case 5:
+                    dataGridView1[1, 2].Value = tmpN;
+                    break;
+                case 6:
+                    dataGridView1[2, 2].Value = tmpN;
+                    break;
+                case 7:
+                    dataGridView1[0, 3].Value = tmpN;
+                    break;
+                case 8:
+                    dataGridView1[1, 3].Value = tmpN;
+                    break;
+                case 9:
+                    dataGridView1[2, 3].Value = tmpN;
+                    break;
+            }
+            tmpN = String.Empty;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.CellBorderStyle = DataGridViewCellBorderStyle.None;
+            for (byte b=0; b < 5; b++)
+            dataGridView1.Rows.Add();
+        }
+    }
+}
